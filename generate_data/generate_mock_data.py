38845bce5eecb1fb7562152d608bd1bf233160c1
ashishpanchal <apanchal8746@sdsu.edu>
2024-03-26 23:52:04 -0700

    fixed code for lambda

--- generate_data/generate_mock_data.py
+++ generate_data/generate_mock_data.py
@@ -81,7 +81,7 @@ def write_to_csv(data, filename):
 def generate_data(current_date, date_str):
     """Generates data and creates csv files"""
     transactions = generate_transactions(transactions_per_day, current_date)
-    write_to_csv(transactions, f"./tmp/transactions_{date_str}.csv")
+    write_to_csv(transactions, f"/tmp/transactions_{date_str}.csv")
 
     print(f"Generated mock transaction data transactions_{date_str}.csv and saved in csv files")
     return
